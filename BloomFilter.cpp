2689fc18d993b1684002217c116091b938eb7e89
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-04-12 21:04:48 -0500

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include "bloomfilter.hpp"
+#include "BloomFilter.hpp"
 
 using namespace std;
 
-int main () {
+int main()
+{
+    BloomFilter filter;
 
+    // Add some values to the filter
+    filter.Add(42);
+    filter.Add(3.14f);
+    filter.Add("hello");
+    filter.Add('c');
+
+    // Check if values are contained in the filter
+    std::cout << "Contains 42: " << filter.Contains(42) << std::endl;
+    std::cout << "Contains 3.14f: " << filter.Contains(3.14f) << std::endl;
+    std::cout << "Contains \"hello\": " << filter.Contains("hello") << std::endl;
+    std::cout << "Contains 'c': " << filter.Contains('c') << std::endl;
+    std::cout << "Contains 'l': " << filter.Contains('l') << std::endl;
+
+    return 0;
 }
