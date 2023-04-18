d52824b049abfa1dacb516514ec0c7e1bc93d40c
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-04-17 19:40:26 -0500

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
-#include "BloomFilter.hpp"
+#include "BloomFilter copy.hpp"
 
 using namespace std;
 
 int main()
 {
-    BloomFilter filter;
+    BloomFilter<int> int_filter;
 
     // Add some values to the filter
-    filter.Add(42);
-    filter.Add(3.14f);
-    filter.Add("hello");
-    filter.Add('c');
+    cout << "Add: 42" << endl;
+    int_filter.Add(42);
+    int_filter.Print();
+    cout << "Add: 89" << endl;
+    int_filter.Add(89);
+    int_filter.Print();
 
     // Check if values are contained in the filter
-    std::cout << "Contains 42: " << filter.Contains(42) << std::endl;
-    std::cout << "Contains 3.14f: " << filter.Contains(3.14f) << std::endl;
-    std::cout << "Contains \"hello\": " << filter.Contains("hello") << std::endl;
-    std::cout << "Contains 'c': " << filter.Contains('c') << std::endl;
-    std::cout << "Contains 'l': " << filter.Contains('l') << std::endl;
+    cout << "Contains 42: " << int_filter.Contains(42) << endl;
+    cout << "Contains 54: " << int_filter.Contains(54) << endl;
+    cout << "Contains 53: " << int_filter.Contains(53) << endl;
+
+    BloomFilter<char *> string_filter;
+
+    char *s1 = "Horse";
+    char *s2 = "Frog";
+    char *s3 = "Girraffe";
+
+    // Add some values to the filter
+    cout << "Add: Horse" << endl;
+    string_filter.Add(s1);
+    string_filter.Print();
+    cout << "Add: Frog" << endl;
+    string_filter.Add(s2);
+    string_filter.Print();
+
+    // Check if values are contained in the filter
+    cout << "Contains Horse: " << string_filter.Contains(s1) << endl;
+    cout << "Contains Girraffe: " << string_filter.Contains(s3) << endl;
 
     return 0;
 }
